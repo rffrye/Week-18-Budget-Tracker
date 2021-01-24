f183b8dd6d56555d09111b705496b535bc0af91c
RFFRYE <rffrye@users.noreply.github.com>
2021-01-23 23:03:05 -0500

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,4 @@
-const FILES_TO_CACHE = [
-    "/", "/index.html","index.js", "/db.js", "/style.css"];
+const FILES_TO_CACHE = ["/", "/index.html","index.js", "/db.js", "/style.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
